d461286387f748af559c903c555c967f04a339ae
Barnald-Bocker <bocker.universidad@gmail.com>
2024-10-15 09:52:35 -0600

    First working version of Capacitor.py, has all the functions and the documentation of the functions. Its missing the other parts of the documentation, as explanation, index, ...

--- src/Capacitor.py
+++ src/Capacitor.py
@@ -11,12 +11,12 @@ def grid_generator(factor = 1, V1 = 1.0, V2 = -1.0, boundary = 0.0):
     Es la función que genera la grilla en la cual se va a trabajar para obtener la solución a la ecuación diferencial. El tamaño de la grilla va a ser de 10*factor filas y 10*factor+1 columnas. La grilla está hecha para manejar condiciones de Dirichlet. El propósito es modelar un capacitor, por eso se van a generar dos placas, una con voltaje V1 ubicada en x = 2 y la otra con voltaje V2 ubicada en x = 8. Asimismo, el borde del capacitor va a tener un voltaje en defecto igual a 0V. El problema trata con una región cuadrada de 10x10 cm, por lo tanto para obtener más precisión se usa el factor de escala ya mencionado.
 
     Examples:
-        factor = 1
-        V1 = 1.0
-        V2 = -1.0
-        boundary = 0.0
+        >>> factor = 1
+        >>> V1 = 1.0
+        >>> V2 = -1.0
+        >>> boundary = 0.0
 
-        >>> print(grid_generator(1.0,1.0,-1.0,0.0))
+        >>> print(grid_generator(factor, V1, V2, boundary))
          [[ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
           [ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
           [ 0.  0.  1.  0.  0.  0.  0.  0. -1.  0.  0.]
@@ -52,15 +52,14 @@ def gauss_seidel(phi, omega, factor, tol):
     Esta es la función que resuelve la ecuación diferencial parcial de Laplace de manera iterativa. En este caso la solución se guarda y se trabaja sobre la misma matriz, lo que es la principal diferencia de los métodos de Jacobi (modificado y simple). Una propiedad que es sumamente importante, es que siempre es estable.
 
     Examples:
-    factor = 1
-    V1 = 1.0
-    V2 = -1.0
-    omega = 0.9
-    tol = 1e-5
+        >>> factor = 1
+        >>> V1 = 1.0
+        >>> V2 = -1.0
+        >>> omega = 0.9
+        >>> tol = 1e-5
 
-    Examples:
-         >>> result, iterations = gauss_seidel( 1, 0.9, 1, 1e-5)
-         >>> print(result)
+        >>> result, iterations = gauss_seidel( 1, 0.9, 1, 1e-5)
+        >>> print(result)
          [[ 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00]
           [ 0.00000000e+00  1.94984598e-01  3.61769011e-01  2.52087439e-01 1.21533562e-01 -1.41308295e-06 -1.21531760e-01 -2.52085676e-01 -3.61767842e-01 -1.94984609e-01  0.00000000e+00]
           [ 0.00000000e+00  4.18168790e-01  1.00000000e+00  5.25040044e-01  2.34042744e-01  1.03625792e-06 -2.34042966e-01 -5.25038493e-01 -1.00000000e+00 -4.18167388e-01  0.00000000e+00]
@@ -109,12 +108,136 @@ def gauss_seidel(phi, omega, factor, tol):
     # Por naturaleza de python, se devuelve en forma de tupla
     return phi, its
 
+def jacobi_relaxation(phi, factor, tol):
+    """
+    Esta es la función que resuelve la ecuación diferencial parcial de Laplace de manera iterativa. El método de relajación de Jacobi es la base para los métodos que se van a utilizar en este proyecto. Usa una discretización de la ecuación diferencial parcial para iterar por la grilla sucesivamente y definir el valor de un punto en específico con base en los puntos que se encuentran alrededor. Una particularidad de los métodos de Jacobi es que calcula los valores y los guarda en una matriz diferente a la que se obtienen estos valores. Este método no siempre es estable.
+
+    Examples:
+        >>> factor = 1
+        >>> V1 = 1.0
+        >>> V2 = -1.0
+        >>> tol = 1e-5
+        >>> phi = grid_generator(factor, V1, V2, boundary)
+
+        >>> result, iterations = jacobi_relaxation(phi, factor, tol )
+        >>> print(result)
+             [[ 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00]
+              [ 0.00000000e+00  1.94982708e-01  3.61765902e-01  2.52080212e-01 1.21529913e-01 -2.84369944e-06 -1.21529913e-01 -2.52083357e-01  3.61765902e-01 -1.94983009e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.18166080e-01  1.00000000e+00  5.25034691e-01 2.34035713e-01  0.00000000e+00 -2.34044624e-01 -5.25034691e-01 -1.00000000e+00 -4.18166080e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.77681614e-01  1.00000000e+00  6.14017698e-01 2.89601333e-01 -6.52821638e-06 -2.89601333e-01 -6.14024255e-01 -1.00000000e+00 -4.77681644e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.92560530e-01  1.00000000e+00  6.41454747e-01 3.10343548e-01 -1.38777878e-17 -3.10355574e-01 -6.41454747e-01 -1.00000000e+00 -4.92560530e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.92560525e-01  1.00000000e+00  6.41451305e-01 3.10349780e-01 -6.45366768e-06 -3.10349780e-01 -6.41457765e-01 -1.00000000e+00 -4.92560532e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.77681632e-01  1.00000000e+00  6.14021179e-01 2.89596565e-01  0.00000000e+00 -2.89605737e-01 -6.14021179e-01 -1.00000000e+00 -4.77681632e-01  0.00000000e+00]
+              [ 0.00000000e+00  4.18166049e-01  1.00000000e+00  5.25032673e-01 2.34040327e-01 -3.70966838e-06 -2.34040327e-01 -5.25036434e-01 -1.00000000e+00 -4.18166101e-01  0.00000000e+00]
+              [ 0.00000000e+00  1.94982876e-01  3.61765564e-01  2.52081888e-01 1.21528167e-01 -3.46944695e-18 -1.21531509e-01 -2.52081888e-01 -3.61766174e-01 -1.94982876e-01  0.00000000e+00]
+              [ 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00 0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00  0.00000000e+00]]
+
+
+    Args:
+        phi (Numpy array): Primer argumento, la grilla 10x10cm escalada por el factor para que sea un numpy array de tamaño (10*factor, 10*factor+1) para modelar el capacitor.
+        V1 (float): Segundo argumento, es el voltaje que corresponde a la primera placa del capacitor.
+        V2 (float): Tercer argumento, es el voltaje que corresponde a la segunda placa del capacitor.
+        factor (int): Cuarto argumento, es el factor de escala para obtener una grilla más detallada. Tiene que ser un entero.
+        tol (float): Quinto argumento, es la tolerancia aceptada para la precisión del problema, se obtiene obteniendo la diferencia máxima entre phi y phi_copy
+
+    Returns:
+        phi (Numpy array): La grilla 10x10cm escalada por el factor para que sea un numpy array de tamaño (10*factor, 10*factor+1) para modelar el capacitor.
+        its (int): Iteraciones que fueron necesarias para llegar a la tolerancia especificada.
+    """
+    # Se asegura que factor sea un int
+    factor = int(factor)
+    # Se crea la copia para guardar los valores que se van obteniendo, esta tiene que tener el mismo formato que phi
+    phi_prime = np.copy(phi)#grid_generator(factor, V1, V2, boundary)
+    # Desarrollo del método Gauss-Seidel
+    # Se inicializa la variable que se usa para el condicional
+    delta = 1.0
+    # Se inicializa el contador de iteraciones
+    its = 0
+    # La escogencia de los parámetros de este for loop tienen un razonamiento. Se puede notar que la matriz especificada tiene tamaño (10*factor, 10*factor+1). Al empezar el for loop en 1 y terminarlo en el tamaño-1 (10*factor-1, 10*factor respectivamente) se evitan las condiciones de frontera que se encuentran en los límites de estas matrices, que serían la primer y última fila y columna, de esta forma se evita el uso de un condicional dentro del ciclo
+    while delta > tol:
+        for i in range(1,10*factor-1):
+            for j in range(1,10*factor):
+                # Condicional que evita las condiciones de frontera
+                if 2*factor <= i < 8*factor and (j == 2*factor or j == 8*factor):
+                    pass
+                else:
+                    # Para cualquier otro caso que no sean las condiciones de frontera, se evalua el algoritmo
+                    phi_prime[i,j] = (0.25)*(phi[i+1,j] + phi[i-1,j] + phi[i,j+1] + phi[i,j-1])
+        # Se calcula la diferencia máxima con respecto a los valores que se obtuvieron en la iteración anterior
+        delta = np.max(np.abs(phi - phi_prime))
+        # Se intercambian ambas grillas para que se siga aplicando el método, para seguir aplicando el delta y mejorar la precisión
+        temp = phi
+        phi = phi_prime
+        # El nuevo phi_prime es el phi viejo
+        phi_prime = temp
+    # En el momento que se cumple la condición en la que delta <= tol, se rompe el ciclo y se devuelve la matriz con la solución phi (en este caso sería la matriz phi_prime) y las iteraciones que fueron requeridas para llegar a la tolerancia mencionada
+    # Por naturaleza de python, se devuelve en forma de tupla
+    return phi_prime, its
+
+def SOR(phi, factor, omega, tol):
+    """
+    Esta es la función que resuelve la ecuación diferencial parcial de Laplace de manera iterativa. El método de relajación de Jacobi es la base para los métodos que se van a utilizar en este proyecto. Usa una discretización de la ecuación diferencial parcial para iterar por la grilla sucesivamente y definir el valor de un punto en específico con base en los puntos que se encuentran alrededor. Una particularidad de los métodos de Jacobi es que calcula los valores y los guarda en una matriz diferente a la que se obtienen estos valores. Este método no es estable para grillas cuadradas
+
+    Examples:
+        >>> factor = 1
+        >>> V1 = 1.0
+        >>> V2 = -1.0
+        >>> omega = 0.9
+        >>> tol = 1e-5
+        >>> phi = grid_generator(factor, V1, V2, boundary)
+
+        >>> result, iterations = SOR(phi, factor, omega,  tol )
+        >>> print(result)
+            RuntimeWarning: overflow encountered in scalar multiply
+            RuntimeWarning: overflow encountered in scalar add
+
+    Args:
+        phi (Numpy array): Primer argumento, la grilla 10x10cm escalada por el factor para que sea un numpy array de tamaño (10*factor, 10*factor+1) para modelar el capacitor.
+        factor (int): Segundo argumento, es el factor de escala para obtener una grilla más detallada. Tiene que ser un entero.
+        omega (float): Tercer argumento, parámetro que depende del problema, dependiendo de su escogencia puede acelerar el cálculo.
+        tol (float): Cuarto argumento, es la tolerancia aceptada para la precisión del problema, se obtiene obteniendo la diferencia máxima entre phi y phi_copy
+
+    Returns:
+        phi (Numpy array): La grilla 10x10cm escalada por el factor para que sea un numpy array de tamaño (10*factor, 10*factor+1) para modelar el capacitor.
+        its (int): Iteraciones que fueron necesarias para llegar a la tolerancia especificada.
+    """
+    # Se asegura que factor sea un int
+    factor = int(factor)
+    # Se crea la copia para guardar los valores que se van obteniendo, esta tiene que tener el mismo formato que phi
+    phi_prime = np.copy(phi)#grid_generator(factor, V1, V2, boundary)
+    # Desarrollo del método Gauss-Seidel
+    # Se inicializa la variable que se usa para el condicional
+    delta = 1.0
+    # Se inicializa el contador de iteraciones
+    its = 0
+    # La escogencia de los parámetros de este for loop tienen un razonamiento. Se puede notar que la matriz especificada tiene tamaño (10*factor, 10*factor+1). Al empezar el for loop en 1 y terminarlo en el tamaño-1 (10*factor-1, 10*factor respectivamente) se evitan las condiciones de frontera que se encuentran en los límites de estas matrices, que serían la primer y última fila y columna, de esta forma se evita el uso de un condicional dentro del ciclo
+    while delta > tol:
+        for i in range(1,10*factor-1):
+            for j in range(1,10*factor):
+                # Condicional que evita las condiciones de frontera
+                if 2*factor <= i < 8*factor and (j == 2*factor or j == 8*factor):
+                    pass
+                else:
+                    # Para cualquier otro caso que no sean las condiciones de frontera, se evalua el algoritmo
+                    phi_prime[i,j] = (1.0+omega)*((phi[i+1,j] + phi[i-1,j] + phi[i,j+1] + phi[i,j-1]))*0.25 - omega*phi[i,j]
+        # Se calcula la diferencia máxima con respecto a los valores que se obtuvieron en la iteración anterior
+        delta = np.max(np.abs(phi - phi_prime))
+        # Se intercambian ambas grillas para que se siga aplicando el método, para seguir aplicando el delta y mejorar la precisión
+        temp = phi
+        phi = phi_prime
+        # El nuevo phi_prime es el phi viejo
+        phi_prime = temp
+    # En el momento que se cumple la condición en la que delta <= tol, se rompe el ciclo y se devuelve la matriz con la solución phi (en este caso sería la matriz phi_prime) y las iteraciones que fueron requeridas para llegar a la tolerancia mencionada
+    # Por naturaleza de python, se devuelve en forma de tupla
+    return phi_prime, its
 factor = 10
-omega = 0.9
+V1 = 1.0
+V2 = -1.0
 tol = 1e-5
-main_grid = grid_generator(factor)
-result, iterations = gauss_seidel(main_grid, omega, factor, tol)
+boundary = 0.0
+omega = 0.9
+main_grid = grid_generator(factor, V1, V2, boundary)
+result, iterations = SOR(main_grid, factor, omega, tol)
 plt.imshow(result, cmap = "gray")
 plt.tight_layout()
-plt.title(f"Gráfico para omega = {0.9}")
 plt.show()
