09282cfe85e49ab2079b629d4c2e2eb897bbb47d
Barnald-Bocker <bocker.universidad@gmail.com>
2024-12-02 11:28:53 -0600

    Trying stuff in distributed memory

--- src/parallel_cpp/distributed_memory/Capacitor_ghost.cpp
+++ src/parallel_cpp/distributed_memory/Capacitor_ghost.cpp
@@ -1,138 +1,158 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <sys/time.h>
-#include <omp.h>
+#include <mpi.h>
 
-// Función generadora de la grilla inicial.
-std::vector<double> grid_generator(int factor, double V1, double V2, double boundary = 0.0){
-
-	// Manejo de excepciones para evitar factores de escala no válidos.
-	if(factor < 1){
-	    std::cerr << "El factor de escala debe ser un número entero mayor que cero!" << std::endl;
-	    exit(1);
-	}
-
-	// Generar la grilla con las dimensiones (10*factor x 10*factor+1) y las líneas de potencial con V1, V2
-	int column = 10*factor+1;
-	std::vector<double> phi(10*factor*column); 
-	for(int i=2*factor; i < 8*factor; ++i){
-		phi[i*column+2*factor] = V1;
-		phi[i*column+8*factor] = V2;
-	}
-
-	return phi;
-}
+int gauss_seidel(double V1, double V2, double omega, int factor, double tolerance){
 
 
-// Función que regresa la medición del tiempo actual.
-double seconds(){
-	struct timeval tmp;
-	double sec;
-	gettimeofday( &tmp, (struct timezone *)0 );
-	sec = tmp.tv_sec + ((double)tmp.tv_usec)/1000000.0;
-
-	return sec;
-
-}
-
-// Función que se encarga de las iteraciones del método de Gauss-Seidel.
-// Como variables de entrada se encuentra una referencia a la grilla, el valor 'omega', el factor de escala y la tolerancia con la que se desea evaluar.
-int gauss_seidel(std::vector<double>& phi, double omega, int factor, double tolerance){
-
-	// Manejo de excepciones, evitar que el factor de escala ingresado en este método sea diferente al utilizado al momento de generar la grilla.
-	if(phi.size() != (10*factor)*(10*factor+1)){
-		std::cerr << "El factor de escala ingresado es diferente al utilizado para generar la grilla!" << std::endl;
-		exit(1);
-	}
-
-	// Manejo de excepciones para asegurar que el valor de tolerancia sea un número válido.
 	if(tolerance <= 0){
 		std::cerr << "El valor de tolerancia no puede ser nulo ni negativo!" << std::endl;
 		exit(1);
 	}
-
-	// Se realiza un deep copy de la grilla. Nótese que la sintaxis es de una copia superficial, sin embargo, la biblioteca 'vector' se encarga de gestionar esta operación como una copia profunda.
-	std::vector<double> phiCopy = phi;
 	double delta = 1.0;
 	double diff;
 	double maxDelta;
 	int its = 0;
-	// Se define la variable 'column' para simplificar la apariencia del código.
 	int column = 10*factor+1;
-	int initValue;
+  int size, rank;
+  int row = 10*factor;
+  double time_1 = MPI_Wtime();
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  int nlocal = row/size;
+  int rest = row%size;
+  
+  if (rest && rank<rest){
+    nlocal ++;
+  }
+  
+  int start = nlocal*rank;
+  if (rest && rest <= rank){
+    start += rest;
+  }
+  
+  //int end = start +  nlocal;
+  int end = nlocal+1;
+ 
+  std::vector<double> local_section;
 
-	// Se inicia el contador de tiempo antes de iniciar las iteraciones para poder medir la escalabilidad de la paralelización.
-	double time_1 = seconds();
-	int num_procs;
+  if(rank == 0 || rank == size - 1){
+    local_section.resize((nlocal+1)*column); // MUY IMPORTANTE: GRILLAS FANTASMA 
+    end = nlocal;
+  }
+  else{
+    local_section.resize((nlocal+2)*column);
+  }
+  
+  if (rank == size-1){
+    end = nlocal-1;
+  }
+  
+  //Establecer condiciones de frontera
+  
+  for(int i=1; i < end; ++i){
+    int absolute_position = start+i-1;
+    if (rank == 0){
+      absolute_position = start + i;
+    }
+    for(int j=1; j < column-1; ++j){
+      if ((2*factor <= absolute_position && absolute_position < 8*factor) && (j == 2*factor)){
+        local_section[i*column + j] = V1;
+      }
+      if ((2*factor <= absolute_position && absolute_position < 8*factor) && (j== 8*factor)){
+        local_section[i*column + j] = V2;
+      }
+    }
+  }
+  
+  // Envio de las filas fantasma superiores.
+  if (rank > 0){
+    MPI_Request request_send, request_recv;
+    MPI_Isend(&local_section[column], column, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &request_send);
+    MPI_Irecv(&local_section[end*column], column, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, &request_recv);
+    MPI_Wait(&request_send, MPI_STATUS_IGNORE);  // Wait for send to complete
+    MPI_Wait(&request_recv, MPI_STATUS_IGNORE);  // Wait for receive to complete
+  }
 
-	// Vamos a recorrer la grilla por líneas diagonales, empezando por un primer ciclo encargado de ir variando las diagonales de tal forma que las entradas iniciales de cada diagonal se vayan desplazando a lo largo de la primera columna. Una vez se alcance la última fila de la primera columna, se realizará un desplazamiento de la entrada inicial de cada diagonal a lo largo de toda la fila mencionada, que corresponde al segundo ciclo.
-	while(delta > tolerance){
-		
-		// Se establece 'initValue = 1' para el primer ciclo. Corresponderá a la columna que posee las entradas iniciales de las diagonales.
-		initValue = 1;
-		// Primer ciclo. initValue se encargará de iniciar las diagonales en la segunda columna de la grilla. Se evita la primera columna por las condiciones de frontera.
-		// Inicializamos el ambiente OMP.
-		#pragma omp parallel
-		{
-		num_procs = omp_get_num_threads();
-		
-		for(int i=1; i < 10*factor-1; ++i){
-			// Se paraleliza el recorrido para cada línea diagonal. (Fork_1)
-			#pragma omp for
-			for(int d=0; d < i; ++d){ // 'd' corresponde al ciclo que recorre las líneas diagonales.
-			// Nótese que ahora las entradas de la grilla corresponden a '[i-d,j+d]'.
-			// Para el primer ciclo, j->initValue. Por lo tanto: '[i-d,initValue+d]'.
-			// El valor 'd' se resta a la fila y suma a la columna porque el objetivo es generar líneas diagonales que se recorran con monotonía creciente.
-				if((2*factor <= (i-d) && (i-d) < 8*factor) && ((initValue+d) == 2*factor || (initValue+d) == 8*factor)){
-					continue;
-				} else{
-					phi[(i-d)*column + (initValue+d)] = ((1.0+omega)*0.25)*(phi[(i-d+1)*column + (initValue+d)] + phi[(i-d-1)*column + (initValue+d)] + phi[(i-d)*column + (initValue+d+1)] + phi[(i-d)*column + (initValue+d-1)]) - omega*phi[(i-d)*column + (initValue+d)];
-				}
-			}
-		}
-		} // Se cierra el ambiente OMP para el primer ciclo. (Join_1)
+  if (rank + 1 < size){
+    MPI_Request request_send, request_recv;
+    MPI_Isend(&local_section[end*column], column, MPI_DOUBLE, rank+1, 1, MPI_COMM_WORLD, &request_send);
+    MPI_Irecv(&local_section[0], column, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, &request_recv);
+    MPI_Wait(&request_send, MPI_STATUS_IGNORE);  // Wait for send to complete
+    MPI_Wait(&request_recv, MPI_STATUS_IGNORE);  // Wait for receive to complete
+  }
+  // Copia
+  std::vector<double> local_sectionCopy = local_section;
+  
+  while(delta > tolerance){
+    for(int i=1; i < end; ++i){
+      int absolute_position = start+i-1;
+      if (rank == 0){
+        absolute_position = start + i;
+      }
+      for(int j=1; j < column-1; ++j){
+        if((2*factor <= absolute_position && absolute_position < 8*factor) && (j == 2*factor || j == 8*factor)){
+  			  continue;
+  			} else{
+  					local_section[i*column + j] = ((1.0+omega)*0.25)*(local_section[(i+1)*column + j] + local_section[(i-1)*column + j] + local_section[i*column + (j+1)] + local_section[i*column + (j-1)]) - omega*local_section[i*column + j];
+  				}
+  			}
+  		}	
+    maxDelta = 0.0;
+    for(int i=1; i < end; ++i){
+       int absolute_position = start+i-1;
+       if (rank == 0){
+         absolute_position = start + i;
+       }
+       for(int j=1; j < column-1; ++j){
+         if((2*factor <= absolute_position && absolute_position < 8*factor) && (j == 2*factor || j == 8*factor)){
+           continue;
+         } else{
+           diff = std::abs(local_section[i*column+j] - local_sectionCopy[i*column+j]);
+           if(diff > maxDelta){
+             maxDelta = diff;
+           }
 
-		// Segundo ciclo, initValue ahora se encargará de iniciar las diagonales en la penúltima fila de la grilla. Se evita la última fila por las condiciones de frontera.
-		// Se establece 'initValue = 10*factor-2' para el segundo ciclo. Corresponderá a la fila que poseerá las entradas iniciales de las diagonales.
-		initValue = 10*factor-2;
-		// Se inicializa el ambiente OMP para el segundo ciclo. (Fork_2)
-		#pragma omp parallel
-		{
-		for(int j=2; j < 10*factor; ++j){
-			// Se paraleliza el recorrido para cada línea diagonal.
-			#pragma omp for
-			for(int d=0; d < 10*factor-j; ++d){
-			// Nótese que, nuevamente, las entradas de la grilla corresponden a '[i-d,j+d]'.
-			// Para el segundo ciclo, i->initValue. Por lo tanto: '[initValue-d,j+d]'.
-				if((2*factor <= (initValue-d) && (initValue-d) < 8*factor) && ((j+d) == 2*factor || (j+d) == 8*factor)){
-                                        continue;
-                                } else{
-                                        phi[(initValue-d)*column + (j+d)] = ((1.0+omega)*0.25)*(phi[(initValue-d+1)*column + (j+d)] + phi[(initValue-d-1)*column + (j+d)] + phi[(initValue-d)*column + (j+d+1)] + phi[(initValue-d)*column + (j+d-1)]) - omega*phi[(initValue-d)*column + (j+d)];
-				}
-			}
-		}
-		} // Se cierra el ambiente OMP para el segundo ciclo. (Join_2)
-		
-		maxDelta = 0.0;
-		for(int i=1; i < 10*factor-1; ++i){
-			for(int j=1; j < column-1; ++j){
-				diff = std::abs(phi[i*column + j] - phiCopy[i*column + j]);
-				if(diff > maxDelta){
-					maxDelta = diff;
-				}
-			}
-		}
+          }
+        }
+    }
+    double global_delta;
+    MPI_Allreduce(&maxDelta, &global_delta, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    delta = global_delta;
 
-		delta = maxDelta;
-		phiCopy = phi;
-		its += 1;
-	}
+      // Comunicar el delta
 
-	double time_2 = seconds();
-	std::cout << "Número de procesos: " << num_procs << std::endl;
-	std::cout << "Tiempo transcurrido: " << time_2 - time_1 << std::endl;
-	return its;
+    // Envio de las filas fantasma superiores.
+    // Envio de las filas fantasma superiores.
+    if (rank > 0){
+      MPI_Request request_send, request_recv;
+      MPI_Isend(&local_section[column], column, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &request_send);
+      MPI_Irecv(&local_section[end*column], column, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &request_recv);
+      MPI_Wait(&request_send, MPI_STATUS_IGNORE);  // Wait for send to complete
+      MPI_Wait(&request_recv, MPI_STATUS_IGNORE);  // Wait for receive to complete
+    }
+    if (rank == 0){
+  
+    }
+    if (rank + 1 < size){
+      MPI_Request request_send, request_recv;
+      MPI_Isend(&local_section[end*column], column, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, &request_send);
+      MPI_Irecv(&local_section[0], column, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, &request_recv);
+      MPI_Wait(&request_send, MPI_STATUS_IGNORE);  // Wait for send to complete
+      MPI_Wait(&request_recv, MPI_STATUS_IGNORE);  // Wait for receive to complete
+    }
+    // Copia
+    local_sectionCopy = local_section;
+    its += 1;
+  }
+  //std::cout << delta << std::endl;
+  
+  double time_2 = MPI_Wtime();
+  MPI_Finalize(); 
+  return its;
 }
 
 void print_grid(std::vector<double> &matrix, int factor){
@@ -149,12 +169,10 @@ void print_grid(std::vector<double> &matrix, int factor){
   }
 }
 int main(){
-  int factor = 50;
+  int factor = 1;
   double volt1 = 1.0;
   double volt2 = -1.0;
-  std::vector<double> grid = grid_generator(factor, volt1, volt2);
   double omega = 0.9;
   double tolerance = 1e-4;
-  int iterations = gauss_seidel(grid, omega, factor, tolerance);
-  //print_grid(grid, factor);
+  int iterations = gauss_seidel(volt1,volt2,omega, factor, tolerance);
 }
